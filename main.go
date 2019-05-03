86f1b93d1c4f75993a99cedd4811f5b56fd91b0e
Sedat Can Yalçın <sedatcyalcin@gmail.com>
2019-05-03 15:29:14 +0300

    code check

--- main.go
+++ main.go
@@ -1,36 +1,42 @@
 package main
 
 import (
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
+	"strings"
+	"time"
 )
 
-var rounds string
+var rounds int
 var sessionToken string
 var gameRound int
 var totalRounds int
 var gameScore = make(map[string]int)
-var roundResults = make(map[int]string)
+var roundResults = []string{}
 
 var types = map[int]string{
-	0: "rock",
-	1: "paper",
-	2: "scissors",
+	0: "ROCK",
+	1: "PAPER",
+	2: "SCISSORS",
 }
 
 func main() {
 	http.HandleFunc("/newGame", newGameHandler)
 	http.HandleFunc("/play", playHandler)
-	http.ListenAndServe(":8080", nil)
+
+	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
 
 func newGameHandler(w http.ResponseWriter, req *http.Request) {
-	if rounds = req.URL.Query().Get("rounds"); rounds != "" {
-		totalRounds, _ = strconv.Atoi(rounds)
+	if rounds, _ = strconv.Atoi(req.URL.Query().Get("rounds")); rounds > 0 {
+		rand.Seed(time.Now().UnixNano())
 		sessionToken = strconv.Itoa(rand.Intn(100))
 
-		roundResults = make(map[int]string)
+		roundResults = make([]string, 0)
 		gameRound = 0
 		gameScore["player"], gameScore["computer"] = 0, 0
 
@@ -46,56 +52,59 @@ func newGameHandler(w http.ResponseWriter, req *http.Request) {
 			"\nto play Scissors: http://localhost:8080/play?choose=scissors&id=" + sessionToken + "\n"))
 
 	} else {
-		w.Write([]byte("Rounds cannot be empty!"))
+		w.Write([]byte("Rounds input in wrong format\n"))
 	}
 
 }
 
 func playHandler(w http.ResponseWriter, req *http.Request) {
+	myChoice := strings.ToUpper(req.FormValue("choose"))
+	if getIndex(myChoice) == -1 {
+		w.Write([]byte("Choose is in wrong format\n"))
+		return
+	}
 
-	if req.URL.Query().Get("id") == sessionToken && gameRound < totalRounds {
-		againstIndex := rand.Intn(3)
+	if req.URL.Query().Get("id") == sessionToken && gameRound < rounds {
 		gameRound++
-		outp := "-> ROUND " + strconv.Itoa(gameRound) + "\n\nme: " + req.FormValue("choose") + "\nyou: " + types[againstIndex] + "\n"
-		outp += decideWinner(req.FormValue("choose"), types[againstIndex])
-		roundResults[gameRound] = req.FormValue("choose") + " vs " + types[againstIndex]
-		if gameRound == totalRounds {
+		againstIndex := rand.Intn(3) // Karşı el
+		outp := "-> ROUND " + strconv.Itoa(gameRound) + "\n\nme: " + types[againstIndex] + "\nyou: " + myChoice + "\n"
+		outp += decideWinner(myChoice, types[againstIndex])
+		roundResults = append(roundResults, types[againstIndex]+" vs "+myChoice)
+		if gameRound == rounds {
 			outp += "\n-> GAME COMPLETED\n"
 			for k, v := range roundResults {
-				outp += "Round " + strconv.Itoa(k) + ": " + v + "\n"
+				outp += "Round " + strconv.Itoa(k+1) + ": " + v + "\n"
 			}
-			outp += strconv.Itoa(gameScore["computer"]) + " vs " + strconv.Itoa(gameScore["player"])
+			outp += "\n" + strconv.Itoa(gameScore["computer"]) + " vs " + strconv.Itoa(gameScore["player"])
 			if gameScore["player"] > gameScore["computer"] {
 				outp += "\nYOU WON\n"
-			}
-			if gameScore["player"] < gameScore["computer"] {
+			} else if gameScore["player"] < gameScore["computer"] {
 				outp += "\nYOU LOST\n"
 			} else {
 				outp += "\nIT'S A TIE\n"
 			}
 		} else {
-			outp += "\nThere is " + strconv.Itoa((totalRounds - gameRound)) + " more round\n"
+			outp += "\nThere is " + strconv.Itoa((rounds - gameRound)) + " more round.\n"
 		}
 		w.Write([]byte(outp))
 	} else {
-		w.Write([]byte("New game hasn't started or wrong session id"))
+		w.Write([]byte("New game hasn't started or wrong session id\n"))
 	}
 }
 
-func decideWinner(chosen string, against string) string {
+func decideWinner(chosen, against string) string {
 	choice := getIndex(chosen)
 	versus := getIndex(against)
 
 	if (choice+1)%3 == versus {
 		gameScore["computer"]++
 		return "\nYOU LOST THIS ROUND\n"
-	}
-	if choice == versus {
+	} else if choice == versus {
 		return "\nTIE THIS ROUND!!\n"
-	} else {
-		gameScore["player"]++
-		return "\nYOU WON THIS ROUND!!\n"
 	}
+	gameScore["player"]++
+	return "\nYOU WON THIS ROUND!!\n"
+
 }
 
 func getIndex(word string) int {
